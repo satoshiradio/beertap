dacea767d507e173ce863fd375d9b986fe03f6bc
StijnBTC <10080035+StijnBTC@users.noreply.github.com>
2022-11-10 17:03:50 +0100

    added pour time

--- settings.py
+++ settings.py
@@ -4,16 +4,20 @@ env = Env()
 env.read_env()
 
 DEBUG = env.bool("DEBUG", default=False)
-EMULATE_GPIO = env.bool("EMULATE_GPIO", default=False)
 FULLSCREEN = env.bool("FULLSCREEN", default=True)
 
+EMULATE_GPIO = env.bool("EMULATE_GPIO", default=False)
 GPIO_TAP_PIN = env.int("GPIO_TAP_PIN")
 
+LIGHTNING_BACKEND = env.str("LIGHTNING_BACKEND", default=None)
+
+MILLILITER_PER_HOUR = env.int("MILLILITER_PER_HOUR")
+MILLILITER_PER_GLASS = env.int("MILLILITER_PER_GLASS")
+TAP_TIME_ADJUSTMENT_SECONDS = env.int("TAP_TIME_ADJUSTMENT_SECONDS", default=0)
 PRICE_IN_EUROCENTS = env.int("PRICE_IN_EUROCENTS")
 
 LNBITS_BASE_URL = env.str("LNBITS_BASE_URL")
 LNBITS_INVOICE_KEY = env.str("LNBITS_INVOICE_KEY")
-LIGHTNING_BACKEND = env.str("LIGHTNING_BACKEND", default=None)
 
 
 
