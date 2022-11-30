9bc3ff9cc53439aee037c837f3204e420e86940b
Jesse de Wit <witdejesse@hotmail.com>
2022-12-01 00:15:35 +0100

    bootstrap the new controllers

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
+import asyncio 
 from actuators.gpio import GPIOActuator
 from controller.payment_controller import PaymentController
+from controller.nfc_controller import NfcController
+from controller.lnurlw_controller import LnurlwController
 from utils.EventChannel import EventChannel
-
 
 class App:
     def __init__(self):
         self.event_channel = EventChannel()
         self.paymentController = PaymentController(self.event_channel)
         self.actuator = GPIOActuator(self.event_channel)
+        self.nfcController = NfcController(self.event_channel)
+        self.lnurlwController = LnurlwController(self.event_channel)
 
     async def exec(self):
         await self.paymentController.generate_invoice()
         self.paymentController.check_payments()
+        asyncio.get_event_loop().create_task(self.nfcController.listen())
